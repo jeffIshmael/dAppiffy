3b3382f2ce6bdced8c66d26cc284595522da70fd
jeffIshmael <jeffishmael141@gmail.com>
2024-05-28 19:23:27 +0300

    frontend connect

--- frontend/app/Register-dApp/page.tsx
+++ frontend/app/Register-dApp/page.tsx
@@ -1,9 +1,21 @@
-import React from 'react'
+import React from "react";
+import dAppifyABI from "@/components/Blockchain/dAppifyABI.json";
+import { Toaster, toast } from "sonner";
+import { useReadContract, useAccount } from "wagmi";
+import { useWeb3Modal } from "@web3modal/wagmi/react";
 
 export const RegisterdApp = () => {
-  return (
-    <div>RegisterdApp</div>
-  )
-}
+  const { open } = useWeb3Modal();
+  open();
+  const { data : boolean } = useReadContract({
+    abi : dAppifyABI,
+    address:  "0x03bD673B77C9f21F1F1C74c697D5c8E9e77EC432",
+    functionName: 'login',
+    args: ['small'],
+    
+  })
+  
+  return <div>data</div>;
+};
 
-export default RegisterdApp
+export default RegisterdApp;
